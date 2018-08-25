0f873a8e563fc9d32e5c42d85d3c02c9c41c0f03
aleks2414 <alejandroromo14@gmail.com>
2018-08-25 16:02:09 -0500

    landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+
+def quimicos_basicos
+	
+end
+
+
+def especialidades
+	
+end
+
+def cuidado_personal
+	
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :contacts, only: [:new, :create]
+  get 'quimicos_basicos' => 'pages#quimicos_basicos'
+  get 'especialidades' => 'pages#especialidades'
+  get 'cuidado_personal' => 'pages#cuidado_personal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
